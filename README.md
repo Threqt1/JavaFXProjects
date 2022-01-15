2b012d9fe36830c92e7347fbeee5475ce7acc6cc
Threqt1 <threqt_@outlook.com>
2022-01-15 12:33:42 -0600

    Finished project

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 A small application to preview changes in border radius
 
 # Learned
-- Still Building This
+- Editing styles with JavaFX
+- Learned about listening to value changes (slider valueProperty)
+- Handling events with lambdas instead of classes with a changed method
--- src/main/java/practice/javafx/borderradiuspreviewer/BorderRadiusPreviewerMainController.java
+++ src/main/java/practice/javafx/borderradiuspreviewer/BorderRadiusPreviewerMainController.java
@@ -1,13 +1,30 @@
 package practice.javafx.borderradiuspreviewer;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.shape.Rectangle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BorderRadiusPreviewerMainController implements Initializable {
+    @FXML
+    Rectangle box;
+
+    @FXML
+    Slider slider;
+
+    @FXML
+    Label radius;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        slider.valueProperty().addListener((listener, oldValue, newValue) -> {
+            int newRadius = newValue.intValue();
+            radius.setText("Current Radius: " + newRadius);
+            box.setStyle("-fx-arc-height: " + newRadius + ";" + "-fx-arc-width: " + newRadius + ";");
+        });
     }
 }
